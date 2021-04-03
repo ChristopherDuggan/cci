cf9a180e432195337a091476a9dc49b915c64495
Christopher Duggan <christopherduggan@gmail.com>
2021-04-02 20:16:30 -0400

    01/01 solve without data structures, add big O

--- js/01/01-no-additional-data-structures.js
+++ js/01/01-no-additional-data-structures.js
@@ -1,17 +1,17 @@
-//assuming standard ASCII. Change '128's to match chosen character encoding
+//assuming standard ASCII. Change '128' to match chosen character encoding
 function isUnique(string) {
   if (string.length > 128) return false; //quick check for huge strings
-  let charArray = new Array(128);
 
-  for (let i = 0; i < string.length; i++ ) {
-    if (charArray [string.charCodeAt(i)]) return false;
-    charArray[string.charCodeAt(i)] = true;
+  for (let i = 0; i < string.length - 1; i++) {
+    for (let j = i + 1; j < string.length; j++) {
+      if (string.charAt(i) === string.charAt(j)) return false;
+    }
   }
 
   return true;
 }
 
-// O(n) time, O(1) space
+// O(n^2) time, 0(1) space
 
 console.log('string "abcd":', isUnique( 'abcd'));
 console.log('string "     ":', isUnique( '     '));
